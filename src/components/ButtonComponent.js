36d7df02bc1a4616c0dd9d668df17e03799e6e4a
Ricardo Federico Barzola <fedebarzola7@gmail.com>
2023-02-07 23:26:19 -0300

    empezamos a programar con react

---
+++ src/components/ButtonComponent.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const ButtonComponent = () => {
+  return (
+    <div>
+    <div id="blurElements" className="background"></div>
+    
+    <div className="moviles">
+        <button className="boton1" id="openBtn">SOBRE MI</button>
+        <button className="close" id="closeBtn">X</button>
+
+        <button className="boton2" id="openBtn2">HABILIDADES</button>
+        <button className="close" id="closeBtn2">X</button>
+
+        <button className="boton3" id="openBtn3">PROYECTOS</button>
+        <button className="close" id="closeBtn3">X</button></div>
+    </div>
+  )
+}
